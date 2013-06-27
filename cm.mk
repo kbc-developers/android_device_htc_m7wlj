a38e46abf3f3e9699efe8d6349fc17d63e904ac2
tako0910 <tako.oct0910@gmail.com>
2013-06-28 01:56:23 +0900

    Update strings for KDDI m7wlj

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wls BUILD_ID=JZO54K BUILD_FINGERPRINT="htc/m7wls/m7wls:4.1.2/JZO54K/166937.7:user/release-keys" PRIVATE_BUILD_DESC="1.29.651.7 CL166937 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wlj BUILD_ID=JZO54K BUILD_FINGERPRINT="KDDI/m7_jp_kdi/m7wlj:4.1.2/JZO54K/196951.4:user/release-keys" PRIVATE_BUILD_DESC="1.07.970.4 CL196951 release-keys"
 
 PRODUCT_NAME := cm_m7wlj
 PRODUCT_DEVICE := m7wlj
