efd528b8ac3ee52d21f1aa9c0e0645d81fea568e
tako0910 <tako.oct0910@gmail.com>
2014-01-01 18:50:08 +0900

    remove CDMA APN list

--- cm.mk
+++ cm.mk
@@ -1,7 +1,5 @@
 $(call inherit-product, device/htc/m7wlj/full_m7wlj.mk)
 
-$(call inherit-product, vendor/cm/config/cdma.mk)
-
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
