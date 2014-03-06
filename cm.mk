ca169545a9fc98de6725fa86850479cd8eec91ee
tako0910 <tako.oct0910@gmail.com>
2014-03-06 17:52:36 +0900

    m7wlj: Change inherit order  * Move comon_full_phone.mk to top. Otherwise default notification    and alarm tone will be completely wrong
    Please see the following changes for an explanation/source:
    
    http://review.cyanogenmod.org/#/c/59655/
    http://review.cyanogenmod.org/#/c/59973/

--- cm.mk
+++ cm.mk
@@ -1,10 +1,8 @@
-$(call inherit-product, device/htc/m7wlj/full_m7wlj.mk)
-
-$(call inherit-product, vendor/cm/config/cdma.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, device/htc/m7wlj/full_m7wlj.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wlj BUILD_ID=JZO54K BUILD_FINGERPRINT="KDDI/m7_jp_kdi/m7wlj:4.1.2/JZO54K/196951.4:user/release-keys" PRIVATE_BUILD_DESC="1.07.970.4 CL196951 release-keys"
 
