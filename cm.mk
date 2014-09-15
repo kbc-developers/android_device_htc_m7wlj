daeb1fce9d70cdadc75b125b0f9beed008df6306
tako0910 <tako.oct0910@gmail.com>
2014-09-15 13:47:34 +0900

    m7wlj: update fingerprint from OTA 3.13.970.12

--- cm.mk
+++ cm.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/htc/m7wlj/full_m7wlj.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wlj BUILD_ID=JZO54K BUILD_FINGERPRINT="KDDI/m7_jp_kdi/m7wlj:4.1.2/JZO54K/196951.4:user/release-keys" PRIVATE_BUILD_DESC="1.07.970.4 CL196951 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wlj BUILD_ID=KOT49H BUILD_FINGERPRINT="htc/m7_jp_kdi/m7wlj:4.4.2/KOT49H/363468.12:user/release-keys" PRIVATE_BUILD_DESC="3.13.970.12 CL363468 release-keys"
 
 PRODUCT_NAME := cm_m7wlj
 PRODUCT_DEVICE := m7wlj
