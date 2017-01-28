47fc73e2c549b215f753f5cdee0dadd48924f716
tako0910 <tako.oct0910@gmail.com>
2017-01-28 16:16:54 +0900

    m7wlj: Re-add FM radio support

--- device.mk
+++ device.mk
@@ -26,5 +26,11 @@ DEVICE_PACKAGE_OVERLAYS += device/htc/m7-common/overlay-cdma
 # local overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# FM radio
+PRODUCT_PACKAGES += \
+    FM2 \
+    libqcomfm_jni \
+    qcom.fmradio
+
 # Inherit from m7-common
 $(call inherit-product, device/htc/m7-common/m7-common.mk)
