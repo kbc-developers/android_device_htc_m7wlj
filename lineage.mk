7a9a73a742533f37041b7c5dcd6ae2ec58efafdc
tako0910 <tako.oct0910@gmail.com>
2017-02-09 00:00:06 +0900

    m7wlj: Modify model name HTC J One -> HTL22, to fix FeliCa app, etc.

--- lineage.mk
+++ lineage.mk
@@ -21,4 +21,4 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Device naming
 PRODUCT_NAME := lineage_m7wlj
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wlj BUILD_ID=KOT49H BUILD_FINGERPRINT="htc/m7_jp_kdi/m7wlj:4.4.2/KOT49H/363468.12:user/release-keys" PRIVATE_BUILD_DESC="3.13.970.12 CL363468 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=HTL22 BUILD_ID=KOT49H BUILD_FINGERPRINT="htc/m7_jp_kdi/m7wlj:4.4.2/KOT49H/363468.12:user/release-keys" PRIVATE_BUILD_DESC="3.13.970.12 CL363468 release-keys"
